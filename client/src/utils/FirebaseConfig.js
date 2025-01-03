6a1651bfa700dc9691ed0cd0f704b0125e910da3
beteab548 <beteabbaynessagne548@gmail.com>
2025-01-03 14:34:57 -0800

    make some code adjustments

--- client/src/utils/FirebaseConfig.js
+++ client/src/utils/FirebaseConfig.js
@@ -1,13 +1,13 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-const apiKey = process.env.APIKEY;
+console.log(process.env.REACT_APP_APIKEY);
 const firebaseConfig = {
-  apiKey,
+  apiKey: process.env.NEXT_PUBLIC_APIKEY,
+  appId: process.env.NEXT_PUBLIC_APPID,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGINGSENDERID,
   authDomain: "telegram-clone-d89e4.firebaseapp.com",
   projectId: "telegram-clone-d89e4",
   storageBucket: "telegram-clone-d89e4.firebasestorage.app",
-  messagingSenderId: "814999565137",
-  appId: "1:814999565137:web:6dae2f93eb47b118fb5970",
   measurementId: "G-QERPDVWJZ5",
 };
 const app = initializeApp(firebaseConfig);
